565bd1d42a0eb278e5c383ce846273bc1c72bda3
github username <chang.lim92@gmail.com>
2019-08-07 02:27:48 -0400

    TODO: Header

--- src/components/SalePost.js
+++ src/components/SalePost.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import User from './User';
 import { Link } from 'react-router-dom';
 import Image from 'react-image-resizer'
 import PropTypes from 'prop-types';
@@ -142,17 +141,6 @@ class SalePost extends Component{
     })
     }
 
-
-  renderAvatar = () => {
-
-      const {classes} = this.props;
-      if (this.props.renderCategories) {
-        return (
-          <UserBlurb postuser = {this.props.salepost.user} avatar = {classes.avatar} postID = {this.props.salepost.id} renderPosts = {true}/>
-        )
-      }
-  }
-
   render() {
     const { classes } = this.props;
     const { anchorEl } = this.state;
@@ -230,64 +218,6 @@ class SalePost extends Component{
     </Collapse>
   </Card>
 </React.Fragment>
-
-
-    // <Grid item style = {{margin: "auto 8px"}}>
-    // <Card className={classes.card}>
-    //   {this.renderAvatar()}
-    //   <CardActionArea>
-    //     <CardMedia
-    //       component="img"
-    //       className={classes.media}
-    //       table align = 'center'
-    //     />
-    //     <Image
-    //        src= {this.props.salepost.picture_url}
-    //        height={ 300 }
-    //        width={ 300 }
-    //      />
-    //     <CardContent>
-    //       <Typography gutterBottom variant="headline" component="h2">
-    //         {this.props.salepost.name}
-    //       </Typography>
-    //       <div className="meta">
-    //         <span className="price"> {this.props.salepost.price}</span>
-    //         <br />
-    //         <span className="category">  </span>
-    //       </div>
-    //       <Typography component="p">
-    //         {this.generateCategories()}
-    //       </Typography>
-    //     </CardContent>
-    //   </CardActionArea>
-    //   <CardActions>
-    //     <Button size="small" color="primary">
-    //       <FavoriteIcon />
-    //     </Button>
-    //     <Button size="small" color="primary">
-    //       <CommentIcon />
-    //     </Button>
-    //   </CardActions>
-    //   <div className="comment-main-level">
-    //     <div className="comment-box">
-    //       {this.generateComments()}
-    //     </div>
-    //   </div>
-    //   <form onSubmit={this.handleSubmit}>
-    //   <TextField
-    //     className={classes.margin}
-    //     id="input-with-icon-textfield"
-    //     label="TextField"
-    //     value = {this.state.comments}
-    //     onChange = {this.handleComment}
-    //     InputProps={{
-    //       startAdornment: (
-    //   <InputAdornment position="start">
-    //     <AccountCircle />
-    //   </InputAdornment>)}}/>
-    // </form>
-    // </Card>
-    // </Grid>
   );
   }
 }
